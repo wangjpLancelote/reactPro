97c1e3d10f52c954300f5c57e016725ab6e4fa2f
Lorenzo <wjp19951024@163.com>
2021-05-05 14:18:34 +0800

    fix

--- src/components/VideoFrame.jsx
+++ src/components/VideoFrame.jsx
@@ -1,7 +1,7 @@
-import React, {useRef, useEffect, useState, useCallback} from "react";
+import React, {useRef, useEffect, useState, useCallback, useMemo} from "react";
 import styled from 'styled-components'
-import { Image, Tag } from 'antd';
-import './videoFrame.less';
+import { Image, Popover, Spin  } from 'antd';
+import debounce from './useDebounce';
 
 const VideoDebugs = styled.div`
     width: 100%;
@@ -11,45 +11,39 @@ const VideoDebugs = styled.div`
     align-items:center;
     flex-direction:column;
     .debugContainer {
-        width: 600px;
-        height: 360px;
+        width: ${props => props.videoWidth || 600}px;
+        height: ${props => props.videoHeight || 360}px;
         border: 1px solid #000;
         .player{
-            width: 600px;
-            height: 360px;
+            width: 100%;
+            height: ${props => props.videoHeight - 20}px;
             position: relative;
-          
           }
-    }
-    .imageList {
-        width: 100%;
-        height: 360px;
-        border:1px solid #ccc;
-        display:flex;
-        flex-wrap: wrap;
-        overflow:scroll;
-        .imageItem {
-            width: 200px;
-            height: 160px;
-            border-radius: 50%;
-        }
+          .frameControl {
+              width: 100%;
+              height: 20px;
+              background: #ccc;
+              cursor:pointer;
+          }
     }
 `;
 
 function VideoFrame (props) {
-    console.log('ddd', props);
+    const { src } = props;
     const isMultiple = props.multiple ? true : false //是否多选
     const isOnlyChild = React.Children.only(props.children)
     const videoRef = useRef(null);
     const containerRef = useRef(null);
+    const frameControlRef = useRef(null);
+    const [rate, setRate] = useState(0);
     const format = props.format || 'png';
     
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState({});
 
     useEffect(() => {
         videoRef.current.addEventListener('canplay', (e) => {
             videoRef.current.play();
-            videoRef.current.addEventListener('seeked', seekDef)
+            // videoRef.current.addEventListener('seeked', seekDef)
         })
         return videoRef.current.removeEventListener('canplay', () => {
 
@@ -57,39 +51,27 @@ function VideoFrame (props) {
     }, []);
 
     useEffect(() => {
-        videoRef.current.addEventListener('mousemove', (e) => {
-            console.log(e);
+        frameControlRef.current.addEventListener('mousemove', debounce((e) => {
+            const indexTabWidth = e.target.clientWidth;
+            const indexTabOffsetX = e.offsetX;
+            const frameRate = indexTabOffsetX / indexTabWidth;
+            setRate(frameRate);
+            generateFrame(frameRate.toFixed(3));
+    }, 300));
+        return frameControlRef.current.removeEventListener('mousemove', () => {
+            console.log('event mousemove has removed')
         })
-
-        return videoRef.current.removeEventListener('mousemove', () => {
-
-        })
-    }, [])
+    }, []);
 
     const uniq = (arr) => {
         return Array.from(new Set(arr));
     }
 
-    const playVideo = () => {
-        console.log('video', videoRef)
-        if (videoRef.current.paused) {
-            videoRef.current.play();
-            return;
-        }
-        videoRef?.current.pause();
-    }
+    /** 视频寻址 */
     const seekDef = (e) => {
         videoRef.current.removeEventListener('seeked', () => {
             console.log('seeked event has removed');
         });
-        // const video = document.createElement('video');
-        // video.currentTime = videoRef.current.currentTime;
-        // video.volume = videoRef.current.volume;
-        // video.autoplay = true
-        // video.muted = true // most browsers block autoplay unless muted
-        // video.src = videoRef.current.src;
-        // video.setAttribute('crossOrigin', 'anonymous');
-        
         const { image, width, height, currentTime } = generateCanvas(videoRef.current);
         videoRef.current.pause();
         const base64Path = window.URL.createObjectURL(new Blob([image]));
@@ -101,7 +83,21 @@ function VideoFrame (props) {
         });        
     }
 
-    const generateCanvas = (video) => {
+    const generateFrame = (frameRate) => {
+        const { image, width, height, currentTime } = generateCanvas(videoRef.current, videoRef.current.duration * frameRate);
+        const base64Path = window.URL.createObjectURL(new Blob([image]));
+        setImages((prev) => {
+            if (prev.url !== base64Path) {
+                prev = { url: base64Path, time: currentTime, width: width, height: height };
+            }
+            return prev;
+        });
+    }
+
+    const generateCanvas = (video, frameTime) => {
+        console.log('vide', video);
+        video.originCurrentTime = video.currentTime;
+        if (frameTime) video.currentTime = frameTime;
         const canvas = document.createElement('canvas')
         const width = canvas.width = videoRef.current.videoWidth;
         const height = canvas.height = videoRef.current.videoHeight;
@@ -110,39 +106,29 @@ function VideoFrame (props) {
         const data = dataURI.split(',')[1];
         return {
             image: Buffer.from(data, 'base64'),
-            currentTime: videoRef.current.currentTime,
+            currentTime: frameTime,
             width,
             height
         }
     }
 
+    const popoverCard = (
+        <div className="imagePopOver" style={{width: '200px', height: '120px'}}>
+            {
+                images.url ? 
+                <Image src={images.url}/> :
+                <Spin/>
+            }
+        </div>
+    )
+
     return (
         <VideoDebugs>
             <div className="debugContainer" ref={containerRef}>
-                <video ref={videoRef} width="100%" className="player" height="100%" src={require('./test.mp4').default} muted={true} controls crossOrigin="anonymous"/>
-                <div className="control">
-                    <div className="fa fa-play play_pause"></div>
-                    <div>
-                        <span className="progress"></span>
-                    </div>
-                    <div className="timer">
-                        <span className="progress_timer">00:00:00</span>/
-                        <span className="duration_timer">00:00:00</span>
-                    </div>
-                    <div className="fa fa-expand expand"></div>
-                </div>
-            </div>
-            <div className="imageList">
-                {
-                    images.map((item, index) => {
-                        return (
-                            <div className="imageItem" key={index}>
-                                <Image src={item.url}/>
-                                <Tag color="#f50">{item.time}</Tag>
-                            </div>
-                        )
-                    })
-                }                
+                <video ref={videoRef} className="player" src={src ? src : require('./test.mp4').default} muted={true} controls crossOrigin="anonymous"/>
+                <Popover content={popoverCard} placement="top">
+                <div className="frameControl" ref={frameControlRef}></div>
+                </Popover>,
             </div>
         </VideoDebugs>
     )
